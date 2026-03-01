fa88d4d26fd68cb8146e5c12c3405c91786c118f
Guideau24 <burnhorc@gmail.com>
2026-03-01 12:03:27 +0800

    Fix import path for initAntiInspectGuard

--- main.jsx
+++ main.jsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 import theme from "./theme";
-import { initAntiInspectGuard } from "./lib/antiInspect";
+import { initAntiInspectGuard } from "./antiInspect";
 
 initAntiInspectGuard();
 
